2b218b8cad8052c7fe21147246b17b9618dde85a
LSHmain <qhrqn98@daum.net>
2025-08-27 10:09:22 +0900

    chore: normalize line endings with .gitattributes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import fs from "fs";
 
 export default defineConfig({
   plugins: [react()],
@@ -10,6 +11,11 @@ export default defineConfig({
     },
   },
   server: {
+    https: {
+      key: fs.readFileSync("./localhost+2-key.pem"),
+      cert: fs.readFileSync("./localhost+2.pem"),
+    },
     port: 3000,
+    host: "localhost",
   },
 });
