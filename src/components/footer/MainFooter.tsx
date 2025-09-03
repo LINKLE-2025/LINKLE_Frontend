d1d2eee82838e9a6c44945688e447ced1ab5718f
ljhpaul <colosair@gmail.com>
2025-09-03 16:35:19 +0900

    feat: 레이아웃 구조 및 로그인 창 구현
    Added
    - layout 구조
    - 메인 header
    - 메인 footer(임시)

---
+++ src/components/footer/MainFooter.tsx
@@ -0,0 +1,9 @@
+function MainFooter() {
+  return (
+    <footer className='bg-white border-t border-gray-200 text-gray-400 text-xs py-4 text-center fixed bottom-0 w-full'>
+      main footer
+    </footer>
+  );
+}
+
+export default MainFooter;
---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,15 @@
+import MainFooter from "@/components/footer/MainFooter";
+import MainHeader from "@/components/header/MainHeader";
+import { Outlet } from "react-router-dom";
+
+export default function AuthLayout() {
+  return (
+    <div className='flex flex-col min-h-[100dvh] text-black'>
+      <MainHeader />
+      <main className='flex-grow flex flex-col items-center justify-center text-center'>
+        <Outlet />
+      </main>
+      <MainFooter />
+    </div>
+  );
+}
