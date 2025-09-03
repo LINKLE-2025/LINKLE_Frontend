5d66772be0b503c1f178ac65cc78f4b5bd02e391
Ch27s <dbcotmd0@gmail.com>
2025-09-03 16:43:41 +0900

    feat&fix:포스트 페이지 구현 / 사진 순서 변경
    feat
    Added
    - 포스트 페이지 구현
    Fixed
    - 링커 카테고리별 사진 순서 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import LandingPage from "./pages/LandingPage";
 import MapPage from "./pages/MapPage";
 import LoginPage from "./pages/auth/LoginPage";
 import SignUpPage from "./pages/auth/SignupPage";
+import PostCreatePage from "./pages/PostCreatePage";
 // import LoginPage from "./pages/auth/LoginPage";
 // import SignupPage from "./pages/auth/SignupPage";
 
@@ -14,6 +15,7 @@ const App = () => {
       <Route path='/login' element={<LoginPage />} />
       <Route path='/signup' element={<SignUpPage />} />
       <Route path='/map' element={<MapPage />} />
+      <Route path='/post' element={<PostCreatePage />} />
     </Routes>
   );
 };
