a117fc381427a5b69c091ce8335657cd99abf71e
Gaegulin <121297188+Gaegulin@users.noreply.github.com>
2025-09-17 15:20:32 +0900

    fix: 링커생성모드 오류 수정
    fix
    - 링커생성모드 종료해도 링커 생성되는버그 수정

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -850,7 +850,9 @@ export default function MapPage(): React.ReactElement {
       window.history.replaceState({}, document.title);
     }
   }, [location.state, setLinkerCreateMode]);
-
+  useEffect(() => {
+    linkerCreateModeRef.current = linkerCreateMode; // prop으로 받은 값
+  }, [linkerCreateMode]);
 
   useViewportHeight();
   const { headerHeight, footerHeight } = useOutletContext<LayoutContext>();
