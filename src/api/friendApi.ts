a1b11be1da85f992c95b106fc4d0e34db6f8366c
LSHmain <qhrqn98@daum.net>
2025-09-18 11:39:11 +0900

    fix: 친구 리스트 조회
    fix: 친구 리스트 조회

--- src/api/friendApi.ts
+++ src/api/friendApi.ts
@@ -7,6 +7,11 @@ export const getFriends = async (userId: number) => {
     return res.data;
 }
 
+export const getAllFriends = async (userId: number) => {
+    const res = await apiClient.get<FriendResponse[]>(`/friend/list/${userId}`);
+    return res.data;
+}
+
 // 친구 요청
 export const sendFriendRequest = async (userId1: number, userId2: number) => {
     const res = await apiClient.post(`/friend`, { userId1, userId2 });
