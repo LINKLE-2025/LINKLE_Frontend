e9f7223fbe712ecc2e26a2132041a5d80084adbc
ljhpaul <colosair@gmail.com>
2025-09-26 12:11:16 +0900

    style: 링커 검색 및 추천 디자인 개선
    style: 링커 검색 및 추천 디자인 개선

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1147,7 +1147,7 @@ export default function MapPage(): React.ReactElement {
       >
         <Sheet.Container className='z-[0]'>
           <Sheet.Header>
-            <div className='mx-auto my-1 h-1 w-24 rounded-full bg-gray-300' />
+            <div className='mx-auto my-2 h-1.5 w-12 rounded-full bg-gray-300' />
           </Sheet.Header>
           <Sheet.Content>
             <div className='flex flex-col h-full pb-[calc(min(4px,env(safe-area-inset-bottom))+285px)]'>
