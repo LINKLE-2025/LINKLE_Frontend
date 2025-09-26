39f1824770419401a580902743f41bcfb00ff157
ljhpaul <colosair@gmail.com>
2025-09-26 10:34:35 +0900

    feat: 추천 모달 디자인 개선 및 sns 공유용 Open Graph 메타 태그 설정
    Added
    - 추천 모달 디자인 개선
    - sns 공유용 Open Graph 메타 태그 설정

--- src/components/map/AddressDisplay.tsx
+++ src/components/map/AddressDisplay.tsx
@@ -225,8 +225,8 @@ export default function AddressDisplay({
 
             <div>
               {age !== null && gender ? (
-                <div className="flex rounded-sm border-b-2 m-2 py-1 text-xs items-center justify-center">
-                  <LucideWand className="text-[#BA8ED4] mr-2" /> {address}에서 {age}대 {gender}이 많이 찾는 링커 목록입니다.
+                <div className="flex rounded-lg border-2 border-gray-200/40 my-2 mx-3 py-1.5 text-[10px] xxs:text-xs items-center justify-center">
+                  <LucideWand className="w-5 h-5 text-[#BA8ED4] mr-2" /> {address}에서 {age}대 {gender}이 많이 찾는 링커 목록입니다.
                 </div>
               ) : (
                 <div>유저 정보를 불러오는 중...</div>
